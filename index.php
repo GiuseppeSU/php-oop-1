5b49dd0de5b58f6e566fab2333830e739e94dd00
GiuseppeSuanno <suanno.giuseppe@yahoo.it>
2023-05-04 15:08:03 +0200

    defizione classe e dichiarate variabili di istanza

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+class movie
+{
+    public $name;
+    public $language;
+    public $date;
+}
+
+$fantozzi = new movie();
+$fantozzi->name = "Fantozzi";
+$fantozzi->language = "italiano";
+$fantozzi->date = 2000;
